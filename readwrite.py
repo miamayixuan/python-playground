8fb2608739f345feb0838e93e96d8ee3513af964
miamayixuan <mayixuanli@yahoo.com>
2020-01-13 15:56:12 -0500

    add code to read and write text files in python

---
+++ readwrite.py
@@ -0,0 +1,10 @@
+# reads name.txt into a variable my_name 
+with open('name.txt') as f:
+	my_name = f.read()
+
+# writes a new file named hello.txt with the cotents
+with open('hello.txt', 'w') as f:
+    f.write('Hello, my name is ' + my_name)
+
+
+# by Rasim, Mia, and 
