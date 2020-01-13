8ca861e0e348af4a2d47f468c02127230dd92735
miamayixuan <mayixuanli@yahoo.com>
2020-01-13 17:45:51 -0500

    converted superheroes from json to csv

--- superheroes.py
+++ superheroes.py
@@ -1,14 +1,34 @@
+#by Mia and Rasim
+
 #Reads superheroes.json (in this folder)
+
 import json
+import csv
 
 with open('superheroes.json', 'r') as f:
 	superheroes = json.load(f)
 
-powers = []
-members = superheroes['members']
-for member in members:
-	this_members_powers = member['powers']
-	powers.append(this_members_powers)
+# Write a header to the CSV file
+with open('superheroes.csv', 'w') as f:
+    writer = csv.writer(f)
+    headers = ["name", "age", "secretIdentity", "powers", "squadName", "homeTown", "formed", "secretBase", "active"]
+    writer.writerow(headers)
 
-print(powers)
+# Loop over the members
+
+    squad = superheroes['squadName']
+    home = superheroes['homeTown']
+    date = superheroes['formed']
+    secBase = superheroes['secretBase']
+    active = superheroes['active']
+    members = superheroes['members']
+    squad = superheroes['squadName']
+    
+    for member in members:
+    	name = member['name']
+    	age = member['age']
+    	sec_ID = member['secretIdentity']
+    	power = member['powers']
+    	row = [name,age,sec_ID,power,squad,home,date,secBase,active]
+    	writer.writerow(row)
 
